b83f2eb78086d7f8f81cd65600037c8f315e661a
Nikolay Makalov <nexterrible@mail.ru>
2022-11-22 02:23:36 +0700

    Critical delta records detection

--- Energy-Laboratory/formMain.Designer.cs
+++ Energy-Laboratory/formMain.Designer.cs
@@ -33,14 +33,21 @@
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.IndexColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ValueColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DeltaColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DateTimeColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -51,7 +58,7 @@
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.Size = new System.Drawing.Size(962, 571);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -60,7 +67,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(792, 424);
+            this.tabPage1.Size = new System.Drawing.Size(954, 545);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -85,47 +92,98 @@
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
             this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.formsPlot1);
-            this.splitContainer1.Size = new System.Drawing.Size(786, 418);
-            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.Size = new System.Drawing.Size(948, 539);
+            this.splitContainer1.SplitterDistance = 370;
             this.splitContainer1.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(258, 23);
+            this.button1.Size = new System.Drawing.Size(362, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Start reading";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(-1, 103);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(262, 311);
-            this.textBox1.TabIndex = 0;
             // 
             // formsPlot1
             // 
             this.formsPlot1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.formsPlot1.Location = new System.Drawing.Point(0, 0);
             this.formsPlot1.Name = "formsPlot1";
-            this.formsPlot1.Size = new System.Drawing.Size(514, 416);
+            this.formsPlot1.Size = new System.Drawing.Size(572, 537);
             this.formsPlot1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IndexColumn,
+            this.ValueColumn,
+            this.DeltaColumn,
+            this.DateTimeColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(4, 129);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(366, 408);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // IndexColumn
+            // 
+            this.IndexColumn.HeaderText = "Index";
+            this.IndexColumn.Name = "IndexColumn";
+            this.IndexColumn.Width = 50;
+            // 
+            // ValueColumn
+            // 
+            this.ValueColumn.HeaderText = "Value";
+            this.ValueColumn.Name = "ValueColumn";
+            this.ValueColumn.Width = 50;
+            // 
+            // DeltaColumn
+            // 
+            this.DeltaColumn.HeaderText = "Delta";
+            this.DeltaColumn.Name = "DeltaColumn";
+            this.DeltaColumn.Width = 50;
+            // 
+            // DateTimeColumn
+            // 
+            this.DateTimeColumn.HeaderText = "DateTime";
+            this.DateTimeColumn.Name = "DateTimeColumn";
+            this.DateTimeColumn.Width = 170;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(149, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "400";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Порог вхождения по Delta";
             // 
             // formMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(962, 571);
             this.Controls.Add(this.tabControl1);
             this.DoubleBuffered = true;
             this.Name = "formMain";
@@ -138,6 +196,7 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -148,9 +207,15 @@
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private ScottPlot.FormsPlot formsPlot1;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IndexColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ValueColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DeltaColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DateTimeColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Energy-Laboratory/formMain.cs
+++ Energy-Laboratory/formMain.cs
@@ -1,4 +1,5 @@
 ﻿using ScottPlot;
+using ScottPlot.Drawing.Colormaps;
 using ScottPlot.Plottable;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,8 @@ namespace Energy_Laboratory
 {
     public partial class formMain : Form
     {
+        private int _criticalDelta = 400;
+
         public formMain()
         {
             InitializeComponent();
@@ -27,21 +30,50 @@ namespace Energy_Laboratory
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (int.TryParse(textBox1.Text, out int value))
+                _criticalDelta = value;
             ReadCSV(@"..\..\Data\GA03_1003.csv");
         }
 
         private void ReadCSV(string fileName)
         {
+            List<string> criticalDeltas = new List<string>();
+
             using (StreamReader sr = new StreamReader(fileName))
             {
                 string currentLine;
+                string[] parsedLine;
                 int index = 0;
+                double? prevValue = null;
 
                 while ((currentLine = sr.ReadLine()) != null)
                 {
-                    //textBox1.Text += $"{currentLine}\r\n";
-                    //textBox1.Update();
-                    //Text = index++.ToString();
+                    index++;
+                    parsedLine = currentLine.Split(',');
+
+                    if (parsedLine.Length == 2)
+                    {
+                        if (prevValue != null)
+                        {
+                            var delta = Double.Parse(parsedLine[1]) - prevValue;
+
+                            if (delta >= _criticalDelta)
+                                criticalDeltas.Add($"{index} {parsedLine[1]} {delta} {parsedLine[0]}");
+                        }
+
+                        bool result = Double.TryParse(parsedLine[1], out double value);
+                        if (result)
+                            prevValue = value;
+                    }
+                }
+
+                dataGridView1.Rows.Clear();
+                for (int i = 0; i < criticalDeltas.Count; i++)
+                {
+                    var parsedItem = criticalDeltas[i].Split(' ');
+                    dataGridView1.Rows.Add(parsedItem[0], 
+                        parsedItem[1], parsedItem[2], 
+                        parsedItem[3] + parsedItem[4]);
                 }
             }
         }
